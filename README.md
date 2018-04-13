b795306bca96bf4be4a71e4693bd711c5b6c3576
Jason Lee <jason@campus-034-100.ucdavis.edu>
2018-04-12 17:14:50 -0700

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # HackDavis2018
 Chrome Extension - Canvas Calendar
+
+Created a chrome extension to transfer to do list calendar events to Google Calendar. 
+In Alpha, currently desgined to work only with UC Davis Canvas.
+
+Created by @kerbinlee, @medbrown, @itsjasonlee at HackDavis 2018
+
+Finished by @kerbinlee & @itsjasonlee 
--- scripts/background.js
+++ scripts/background.js
@@ -3,6 +3,25 @@
 Goal = Automatically pull date from Canvas's To-Do list and move automatically create calendar events and notifications in Google Calendar
 
 */
+
+chrome.runtime.onMessage.addListener(function(response, sender, sendResponse) {
+    chrome.identity.getAuthToken({ "interactive": true }, function(token) {
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", "https://www.googleapis.com/calendar/v3/calendars/primary/events");
+        xhr.addEventListener("load", function() {
+            //console.log("event added!");
+        });
+        xhr.setRequestHeader("Authorization", "Bearer " + token);
+        xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+        xhr.send(JSON.stringify(response));
+    });
+});
+
+chrome.browserAction.onClicked.addListener(function(activeTab) {
+    //console.log("clicked!");
+});
+
+
 function canvasCalendar() {
     if (window.location.href == "https://canvas.ucdavis.edu/") {
         const length = document.getElementsByClassName("todo-details").length;
@@ -16,15 +35,20 @@ function canvasCalendar() {
             dateField = dateField.split('•'); //Split the dateField into point value + dates
             Description = Description.concat(dateField[0]); //Concat points to descirption
 
-            
+            console.log(Title);
+            console.log(Description);
+
+
             dateField = dateField[1]; //converting date
             dateField = dateField.split(' ');
-            var month = months(dateField[14]);           
+
+            var month = months(dateField[14]);  
+            console.log(month);         
             var day = dateField[15];
+            console.log(day);
             var time = militaryTime(dateField[17]); //Change the time into military format
             var result = '2018-' + month +'-' + day + 'T' + time + ':000' + 'Z'; //Putting the date into the correct format
             var dateField  = result;
-            console.log(dateField);
 
             //Pass the relevant infomation to the Google Calendar API
 
@@ -42,7 +66,23 @@ function months(dateField) {
     else if (dateField == 'Feb')
         return 02;
     else if (dateField == 'Mar')
-        return 03;  
+        return 03;
+    else if (dateField == 'Apr')
+        return 04;  
+    else if (dateField == 'May')
+        return 05;  
+    else if (dateField == 'June')
+        return 06;  
+    else if (dateField == 'July')
+        return 07;  
+    else if (dateField == 'Aug')
+        return 08;  
+    else if (dateField == 'Sept')
+        return 09;  
+    else if (dateField == 'Oct')
+        return 10;
+    else if (dateField == 'Nov')
+        return 11;    
     else
         return 12;
 }
